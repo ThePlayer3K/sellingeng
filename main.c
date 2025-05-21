d98aa37201275cc1486efe8faaea99c395beca11
ThePlayer3K <75815559+ThePlayer3K@users.noreply.github.com>
2025-05-21 22:08:29 +0000

    Feito para o codeblocks, pronto para rodar

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+#include <ctype.h>
+#include <math.h>
+#include <time.h>
+
+int main(void){
+    setlocale(LC_ALL, "Portuguese");
+    system("chcp 1252 > nul");
+    char saida = 'n';
+    do {
+        char opcao;
+        printf("\nOlá! Selecione uma opção abaixo digitando uma letra para continuar");
+        printf("\n(P) Cadastrar, atualizar ou remover produto");
+        printf("\n(I) Iniciar dia de vendas");
+        printf("\n(V) Visualizar dia de vendas anterior");
+        printf("\n(S) Sair");
+        printf("\nSua opção: ");
+        scanf(" %c", &opcao);
+        opcao = toupper(opcao);
+        switch(opcao) {
+            case 'P':
+                /*TODO*/
+                break;
+            case 'I':
+                /*TODO*/
+                break;
+            case 'V':
+                /*TODO*/
+                break;
+            case 'S':
+                printf("\nVocê tem certeza que quer sair? (S/N) ");
+                scanf(" %c", &saida);
+                break;
+            default:
+                system("clear");
+                system("cls");
+                printf("\nOpção inválida, tente novamente");
+        }
+
+    } while (saida != 'S'&& saida != 's');
+    return 0;
+}
